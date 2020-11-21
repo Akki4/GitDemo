a3ba9d4149580cf81de0b2cd75153ae4f831bd6b
Akki4 <AkkijIT12@gmail.com>
2020-11-21 10:54:22 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest
+{
+	@Test
+	public void androidapps()
+	{
+		System.out.println("androidapps");
+	}
+	
+	@Test
+	public void iosapps()
+	{
+		System.out.println("iosapps");	
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postjera()
+	{
+		System.out.println("postjera");
+	}
+	
+	@Test
+	public void twitter()
+	{
+		System.out.println(" twitter");
+	}
+	
+}
